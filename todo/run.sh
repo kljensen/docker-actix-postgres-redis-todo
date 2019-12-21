ac79d739614362e2ee178a57afafa904a74fae66
Kyle L. Jensen <kljensen@gmail.com>
2019-12-20 19:45:52 -0500

    Update deps, fix ports

--- todo/run.sh
+++ todo/run.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 echo -n "Running migrations..."
 diesel database setup
 echo -n "done."
+
+env
+
 if [[ -z "${DEVELOPMENT}" ]]; then
     echo "Running in production!"
 else
     # Systemfd will help us move an open socket to the
     # new process when the process restarts. cargo-watch
     # will start a new process when our code changes.
+    echo "Port = $PORT"
     systemfd --no-pid -s http::0.0.0.0:${PORT} -- cargo watch -x run
 fi
